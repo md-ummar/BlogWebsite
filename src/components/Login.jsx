d99e6825cb30e92e8c0c900e89cf02c86bab3e00
md ummar <mdummar.28@gmail.com>
2024-06-03 14:30:35 +0530

    updated components

---
+++ src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { login as authLogin } from "../store/authSlice";
+import { Button, Input, Logo } from "react-redux";
+import authSlice from "../store/authSlice";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+
+function Login() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState("");
+  return <div>Login</div>;
+}
+
+export default Login;
